07f62fb96f4937c2cb992eb3f3b27f3dbb7929e6
pavel <pavelzavalnyuk11@gmail.com>
2022-11-23 18:49:45 +0300

    Initial commit

---
+++ src/main/java/ru/crbpavel/progressbar/Icons.java
@@ -0,0 +1,8 @@
+package ru.crbpavel.progressbar;
+
+import javax.swing.*;
+
+public interface Icons {
+    ImageIcon MARIO = new ImageIcon(Icons.class.getResource("/sonic.gif"));
+    ImageIcon SHELL = new ImageIcon(Icons.class.getResource("/chanred.gif"));
+}
